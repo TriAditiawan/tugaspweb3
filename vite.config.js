934fabca8b02705198f5357bfde8d04138f4a101
trii <64239221+TriAditiawan@users.noreply.github.com>
2025-11-19 17:18:18 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  
-  plugins: [vue()],
-  base: '/tugaspweb3',   // <--- penting (sesuaikan nama repo)
+  base: '/tugaspweb3/',   // PENTING: harus ada trailing slash
+  plugins: [vue()]
 })
